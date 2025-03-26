07ea9b9d71f092f39f995bac858dc25e06baa55f
VikkyOdessa <vikki.odessa@ukr.net>
2025-03-26 22:31:13 +0000

    Add files via upload

---
+++ exers.py
@@ -0,0 +1,58 @@
+x = [1, 2, 3, 4, 5]
+y = [11, 12, 13, 14, 15]
+z = (21, 22, 23, 24, 25)
+
+"""a = 0 * x
+print("0 * x =", a)
+# a) 0 * x = []
+
+a = x + y
+print("x + y =", a)
+# b) x + y = [1, 2, 3, 4, 5, 11, 12, 13, 14, 15]
+
+[a]
+for item in (x):
+    if item not in y:
+        a.append(item)
+        
+print("x - y =", a)
+# с) x - y = [1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5]
+
+print(x[1]
+)
+# d) 2
+
+print(x[10])
+# e) IndexError: list index out of range
+
+print(y[-1])
+# f) 15
+
+print(y[:])
+# g) 1[11, 12, 13, 14, 15]
+
+print(y[2:4])
+#h) [13, 14]
+
+print(z[1:4:-2]
+#i () Because the step is negative (-2) and the start index is less than the stop index (1 < 4)"""
+
+print(z[:2]
+)
+#j) (21, 22)
+
+print(z[::2]
+)
+#k) (21, 23, 25)
+
+print(z[3]=8)
+#l SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+
+      
+
+
+
+
+
+
+
